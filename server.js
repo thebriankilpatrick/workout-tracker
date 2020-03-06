4534a2a729f686029cfa42cc1876c4bfefb1300e
Brian Kilpatrick <thebriankilpatrick@gmail.com>
2020-03-05 20:09:09 -0500

    Added gitignore, and initial server.js file code

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+// const db = require("./seeders");  ????
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
