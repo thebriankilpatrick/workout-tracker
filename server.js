9288408a12e887ac108f47ec29f7f8dc917fc4a7
Brian Kilpatrick <thebriankilpatrick@gmail.com>
2020-03-07 12:48:32 -0500

    Added workout schema file, as well as request to the exercise html page on button click

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,3 +15,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/custommethods", { useNewUrlParser: true });
+
+app.get("/exercise", ({ body }, res) => {
+    res.sendFile(path.join(__dirname + "/public/exercise.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
